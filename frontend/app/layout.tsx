ad6698306273772efe7059498ad7ff0365939646
Hugo Silva <hugofranciscodasilva24@gmail.com>
2026-08-15 17:10:07 -0300

    Criação do card de login

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../shared/tailwind.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
-      lang="en"
+      lang="pt-br"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,69 +1,40 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+export default function Home(){
+  return(
+    <main className="bg-white h-dvh text-black">
+      <section className="h-full flex justify-center items-center">
+        <div className="w-[35%] shadow-2xl rounded-xl flex flex-col items-center">
+          <section className="flex w-full">
+            <div className="w-1/2 bg-gray-200 text-blue-800 border-b-2 border-blue-800 text-center p-4">
+              <p>Login</p>
+            </div>
+            <div className="w-1/2 border-b-2 border-gray-300 text-center p-4">
+              <p>Cadastro</p>
+            </div>
+          </section>
+          <h1 className="text-4xl font-semibold mt-12 mb-3">Bem-vindo</h1>
+          <p className="font-extralight ">Acesse sua conta para continuar</p>
+          <form className="w-full flex flex-col items-center">
+            <div className="relative w-[80%]">
+              <img className="absolute top-1/2 -translate-y-1/2 z-10 ms-3" src="/icons/mail.svg" alt="Icone de email" />
+              <input className="p-4 w-full bg-gray-100 my-3 ps-12 border-2 border-gray-400 rounded-xl active:outline-gray-600 focus:outline-gray-600" type="email" placeholder="E-mail Intitucional" />
+            </div>
+            <div className="relative w-[80%]">
+              <img className="absolute top-1/2 -translate-y-1/2 z-10 ms-3" src="/icons/lock.svg" alt="Icone de cadeado" />
+              <input className="p-4 w-full bg-gray-100 my-3 ps-12 border-2 border-gray-400 rounded-xl active:outline-gray-600 focus:outline-gray-600" type="password" placeholder="Senha" />
+            </div>
+            <section className="flex justify-between w-[80%] my-3">
+              <div className="space-x-2">
+                <input type="checkbox" id="lembrar-me"/>
+                <label htmlFor="lembrar-me">Lembrar-me</label>
+              </div>
+              <p className="text-blue-900 font-bold">Esqueceu a senha?</p>
+            </section>
+            <button className="bg-blue-700 text-white w-[80%] rounded-xl p-4 cursor-pointer flex justify-center gap-3 mb-5">Entrar <img src="/icons/arrowRight.svg" alt="Icone de seta para a direita" /></button>
+          </form>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+      </section>
+    </main>
+  )
 }
