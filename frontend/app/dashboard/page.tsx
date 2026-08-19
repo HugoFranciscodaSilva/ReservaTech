c0dd32058e0c3765d4c92637157837f794762809
Tumemo <hugo.f.silva15@aluno.senai.br>
2026-08-19 11:49:13 -0300

    Inicio do dashboard(aluno)

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,29 +1,23 @@
-'use client'
-
-import { buttonVariants } from "@/components/ui/button";
-import { Dialog, DialogClose, DialogContent, DialogTrigger } from "@/components/ui/dialog";
-import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
 export default function Dashboard(){
     return(
-        <main className="h-dvh w-full">
-            <Dialog>
-                <DialogTrigger className={buttonVariants({variant:"default"})}>Fazer reserva</DialogTrigger>
-                <DialogContent>
-                    
-                    <DialogClose className={buttonVariants({variant:"ghost"})}>Cancelar</DialogClose>
-                </DialogContent>
-            </Dialog>
+        <main className="h-dvh w-full">           
             <Table>
                 <TableHeader>
                     <TableRow>
+                        <TableHead className="rounded-tl-md">Nome</TableHead>
                         <TableHead></TableHead>
                         <TableHead></TableHead>
                         <TableHead></TableHead>
-                        <TableHead></TableHead>
-                        <TableHead></TableHead>
+                        <TableHead className="rounded-tr-md"></TableHead>
                     </TableRow>
                 </TableHeader>
+                <TableBody>
+                    <TableRow>
+                        
+                    </TableRow>
+                </TableBody>
             </Table>
         </main>
     )
