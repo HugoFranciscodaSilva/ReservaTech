63881a9df70c184d3060c8e2d19063c01858d71e
Hugo Silva <hugofranciscodasilva24@gmail.com>
2026-08-21 15:18:20 -0300

    Criação de interceptador axios

--- frontend/service/axios.ts
+++ frontend/service/axios.ts
@@ -1,6 +1,18 @@
 import axios from 'axios'
 import 'dotenv/config'
+import Cookies from 'js-cookie'
 
 export const API = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL
+})
+
+API.interceptors.request.use((config)=>{
+    const token = Cookies.get('token')
+
+    if(token){
+        config.headers.Authorization = `Bearer ${token}`
+    }
+
+
+    return config
 })
