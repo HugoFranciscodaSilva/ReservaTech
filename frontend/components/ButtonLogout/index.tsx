b221f4edb4c68a39d5c25049bef47cce6413ccde
Hugo Silva <hugofranciscodasilva24@gmail.com>
2026-08-21 15:22:59 -0300

    Criação do logout

--- frontend/components/ButtonLogout/index.tsx
+++ frontend/components/ButtonLogout/index.tsx
@@ -1,8 +1,19 @@
 import { LogOut } from "lucide-react";
 import { Button } from "../ui/button";
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
 
 export default function ButtonLogout(){
+
+    const router = useRouter()
+
+    function Logout(){
+        Cookies.remove('token')
+        alert("Saindo...")
+        router.push('/')
+    }
+
     return(
-        <Button><LogOut/>Fazer logout</Button>
+        <Button className={'absolute bottom-2 right-10'} onClick={Logout}><LogOut/>Fazer logout</Button>
     )
 }
