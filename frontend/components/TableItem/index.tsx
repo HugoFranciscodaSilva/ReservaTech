74cf448ad4c0fb2f919aa5d9a727fde749a2eb87
Hugo Francisco da Silva <hugofranciscodasilva24@gmail.com>
2026-09-03 18:58:13 -0300

    Arredondando tabela de itens

--- frontend/components/TableItem/index.tsx
+++ frontend/components/TableItem/index.tsx
@@ -72,75 +72,79 @@ export default function TableItem(){
     }
 
     return(
-        <Table className="w-full m-auto my-3">
-                <TableHeader>
-                    <TableRow>
-                        <TableHead className="rounded-tl-md px-10 py-5 bg-card text-textoSecundario border-y-2 border-l-2 border-borda">EQUIPAMENTO</TableHead>
-                        <TableHead className="px-10 py-5 bg-card text-textoSecundario border-y-2  border-borda">STATUS</TableHead>
-                        <TableHead className="rounded-tr-md px-10 py-5 bg-card text-textoSecundario border-y-2 border-r-2 border-borda w-[20%]">AÇÕES</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {isError &&
+        <div className="w-full rounded-2xl overflow-hidden border-2 border-borda my-3 m-auto">
+            <Table className="w-full">
+                    <TableHeader>
                         <TableRow>
-                            <TableCell colSpan={3} className="bg-fundoSecundairo text-textoPrincipal px-10 py-5 border-y-2 border-l-2 border-borda text-center">Erro ao carregar itens.</TableCell>
+                            <TableHead className="px-10 py-5 bg-card text-textoSecundario">EQUIPAMENTO</TableHead>
+                            <TableHead className="px-10 py-5 bg-card text-textoSecundario">STATUS</TableHead>
+                            <TableHead className="px-10 py-5 bg-card text-textoSecundario w-[20%]">AÇÕES</TableHead>
                         </TableRow>
-                    }
-                    {isLoading &&
-                        <TableRow>
-                            <TableCell colSpan={3} className="bg-fundoSecundairo text-textoPrincipal px-10 py-5 border-y-2 border-l-2 border-borda text-center">Carregando itens...</TableCell>
-                        </TableRow>
-                    }
-                    {data?.length === 0 &&
-                        <TableRow>
-                            <TableCell colSpan={3} className="bg-fundoSecundairo text-textoPrincipal px-10 py-5 border-y-2 border-l-2 border-borda text-center">Não há itens.</TableCell>
-                        </TableRow>
-                    }
-                    {currentItems?.map((item:itemProps)=>
-                    <TableRow key={item.id}>
-                        <TableCell className="bg-fundoSecundairo text-textoPrincipal px-10 py-5 border-y-2 border-l-2 border-borda">
-                            <div className="flex items-center space-x-7">
-                                <div className={`${item.reserved === "Disponivel" ? "bg-secundaria/20 border-secundaria text-secundaria" : "bg-primaria/20 border-primaria text-primaria"} border rounded-md w-15 h-15 flex items-center justify-center`}>
-                                    {iconMap[item.icon]}
+                    </TableHeader>
+                    <TableBody>
+                        {isError &&
+                            <TableRow>
+                                <TableCell colSpan={3} className="bg-fundoSecundairo text-textoPrincipal px-10 py-5 border-y-2 border-l-2 border-borda text-center">Erro ao carregar itens.</TableCell>
+                            </TableRow>
+                        }
+                        {isLoading &&
+                            <TableRow>
+                                <TableCell colSpan={3} className="bg-fundoSecundairo text-textoPrincipal px-10 py-5 border-y-2 border-l-2 border-borda text-center">Carregando itens...</TableCell>
+                            </TableRow>
+                        }
+                        {data?.length === 0 &&
+                            <TableRow>
+                                <TableCell colSpan={3} className="bg-fundoSecundairo text-textoPrincipal px-10 py-5 border-y-2 border-l-2 border-borda text-center">Não há itens.</TableCell>
+                            </TableRow>
+                        }
+                        {currentItems?.map((item:itemProps)=>
+                        <TableRow key={item.id}>
+                            <TableCell className="bg-fundoSecundairo text-textoPrincipal px-10 py-5 border-y-2 border-borda">
+                                <div className="flex items-center space-x-7">
+                                    <div className={`${item.reserved === "Disponivel" ? "bg-secundaria/20 border-secundaria text-secundaria" : "bg-primaria/20 border-primaria text-primaria"} border rounded-md w-15 h-15 flex items-center justify-center`}>
+                                        {iconMap[item.icon]}
+                                    </div>
+                                    <p className="text-lg">{item.name}</p>
                                 </div>
-                                <p className="text-lg">{item.name}</p>
-                            </div>
-                        </TableCell>
-                        <TableCell className="bg-fundoSecundairo text-textoPrincipal px-10 py-5 border-y-2 border-borda">
-                            {item.reserved === "Disponivel" &&
-                             <div className="flex items-center gap-2">
-                                <div className="w-3 h-3 bg-secundaria rounded-full"></div>
-                                <span className="text-secundaria">{item.reserved}</span>
-                            </div>}
-                            {item.reserved === "Reservado" &&
-                             <div className="flex items-center gap-2">
-                                <div className="w-3 h-3 bg-primaria rounded-full"></div>
-                                <span className="text-primaria">{item.reserved}</span>
-                            </div>
-                             }
-                        </TableCell>
-                        <TableCell  className="bg-fundoSecundairo text-textoPrincipal px-10 py-5 border-y-2 border-r-2 border-borda">
-                            {item.reserved == "Reservado" && <Button className={'w-[80%] bg-fundoPrincipal border-2 border-borda px-8 py-6 flex items-center '} disabled> <LockKeyhole className="text-textoDesabilitado"/> Reservado</Button>}
-                            {item.reserved == "Disponivel" && <Button className={'w-[80%] bg-primaria px-8 py-6 flex items-center justify-between hover:bg-hoverPrimaria'} onClick={() => createReserve(user?.id,item.id)}>Reservar <ArrowRight/> </Button>}
-                        </TableCell>
-                    </TableRow>
-                    )}
-                </TableBody>
-                <TableFooter>
-                    <TableCell colSpan={3} className="px-10 py-5 border-2 border-borda bg-fundoSecundairo text-textoPrincipal">
-                        <div className="flex items-center justify-center">
-                            <button onClick={prevPage} className="cursor-pointer disabled:text-textoDesabilitado" disabled={currentPage ===1}>
-                                <ChevronLeft/>
-                            </button>
-                            {Array.from({length:totalPages}, (_,i) => i+1).map((page) => (
-                                <button className={` w-10 h-10 cursor-pointer rounded-full ${currentPage === page ? "text-textoPrincipal border-2 border-primaria text-lg" : "text-textoDesabilitado"} `} key={page} onClick={() => setCurrentPage(page)}>{page}</button>
-                            ))}
-                            <button onClick={nextPage} className="cursor-pointer disabled:text-textoDesabilitado" disabled={currentPage === totalPages || totalPages === 0}>
-                                <ChevronRight/>
-                            </button>
-                        </div>
-                    </TableCell>
-                </TableFooter>
+                            </TableCell>
+                            <TableCell className="bg-fundoSecundairo text-textoPrincipal px-10 py-5 border-y-2 border-borda">
+                                {item.reserved === "Disponivel" &&
+                                <div className="flex items-center gap-2">
+                                    <div className="w-3 h-3 bg-secundaria rounded-full"></div>
+                                    <span className="text-secundaria">{item.reserved}</span>
+                                </div>}
+                                {item.reserved === "Reservado" &&
+                                <div className="flex items-center gap-2">
+                                    <div className="w-3 h-3 bg-primaria rounded-full"></div>
+                                    <span className="text-primaria">{item.reserved}</span>
+                                </div>
+                                }
+                            </TableCell>
+                            <TableCell  className="bg-fundoSecundairo text-textoPrincipal px-10 py-5 border-y-2 border-borda">
+                                {item.reserved == "Reservado" && <Button className={'w-[80%] bg-fundoPrincipal border-2 border-borda px-8 py-6 flex items-center '} disabled> <LockKeyhole className="text-textoDesabilitado"/> Reservado</Button>}
+                                {item.reserved == "Disponivel" && <Button className={'w-[80%] bg-primaria px-8 py-6 flex items-center justify-between hover:bg-hoverPrimaria'} onClick={() => createReserve(user?.id,item.id)}>Reservar <ArrowRight/> </Button>}
+                            </TableCell>
+                        </TableRow>
+                        )}
+                    </TableBody>
+                    <TableFooter>
+                        <TableRow>
+                            <TableCell colSpan={3} className="px-10 py-5  bg-fundoSecundairo text-textoPrincipal">
+                                <div className="flex items-center justify-center">
+                                    <button onClick={prevPage} className="cursor-pointer disabled:text-textoDesabilitado" disabled={currentPage ===1}>
+                                        <ChevronLeft/>
+                                    </button>
+                                    {Array.from({length:totalPages}, (_,i) => i+1).map((page) => (
+                                        <button className={` w-10 h-10 cursor-pointer rounded-full ${currentPage === page ? "text-textoPrincipal border-2 border-primaria text-lg" : "text-textoDesabilitado"} `} key={page} onClick={() => setCurrentPage(page)}>{page}</button>
+                                    ))}
+                                    <button onClick={nextPage} className="cursor-pointer disabled:text-textoDesabilitado" disabled={currentPage === totalPages || totalPages === 0}>
+                                        <ChevronRight/>
+                                    </button>
+                                </div>
+                            </TableCell>
+                        </TableRow>
+                    </TableFooter>
             </Table>
+        </div>
     )
 }
