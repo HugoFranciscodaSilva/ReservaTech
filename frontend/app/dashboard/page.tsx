6b47ac9343dee847902537dafb17b34f0814d401
Hugo Francisco da Silva <hugofranciscodasilva24@gmail.com>
2026-09-03 19:19:47 -0300

    Tirando o botao antigo de relatorio e historico

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -20,7 +20,6 @@ export default function Dashboard(){
                     <AddItem/>
                 </section>
                 <TableItem/>
-                <ButtonHistory/>
             </main>
         </>
     )
