5d2666e8b757291911267bd3abdf75e14e202f52
Hugo Francisco da Silva <hugofranciscodasilva24@gmail.com>
2026-09-03 18:48:32 -0300

    Paginação entre a tabela

--- frontend/components/TableItem/index.tsx
+++ frontend/components/TableItem/index.tsx
@@ -2,12 +2,12 @@
 
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { Button } from "../ui/button";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "../ui/table";
+import { Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "../ui/table";
 import { API } from "@/service/axios";
 import { itemSchema } from "@/schemas/itemSchema";
 import { useAuthStore } from "@/store/store";
-import { ArrowRight, Camera, Gamepad2, Headset, Keyboard, Laptop, LockKeyhole, Monitor, Mouse, Printer, Projector, RectangleGoggles, Smartphone, TabletSmartphone } from "lucide-react";
-import { ReactNode } from "react";
+import { ArrowRight, Camera, ChevronLeft, ChevronRight, Gamepad2, Headset, Keyboard, Laptop, LockKeyhole, Monitor, Mouse, Printer, Projector, RectangleGoggles, Smartphone, TabletSmartphone } from "lucide-react";
+import { ReactNode, useState } from "react";
 
 interface itemProps extends itemSchema{
     id:number,
@@ -15,6 +15,7 @@ interface itemProps extends itemSchema{
 }
 
 export default function TableItem(){
+
 
     const iconMap:Record<string, ReactNode> = {
         laptop:<Laptop/>,
@@ -54,6 +55,21 @@ export default function TableItem(){
         queryKey:['items'],
         queryFn:fecthItems
     })
+    const [currentPage,setCurrentPage] = useState<number>(1)
+    const itemsPerPages = 3
+    const items = data?.length || 0
+    const totalPages = Math.ceil(items / itemsPerPages)
+    const startIndex = (currentPage -1 ) * itemsPerPages
+    const endIndex = startIndex + itemsPerPages
+    const currentItems = data?.slice(startIndex,endIndex)
+
+    function nextPage(){
+        if(currentPage < totalPages) setCurrentPage((prev) => prev+1)
+    }
+
+    function prevPage(){
+        if(currentPage > 1) setCurrentPage((prev) => prev-1)
+    }
 
     return(
         <Table className="w-full m-auto my-3">
@@ -80,7 +96,7 @@ export default function TableItem(){
                             <TableCell colSpan={3} className="bg-fundoSecundairo text-textoPrincipal px-10 py-5 border-y-2 border-l-2 border-borda text-center">Não há itens.</TableCell>
                         </TableRow>
                     }
-                    {data?.map((item:itemProps)=>
+                    {currentItems?.map((item:itemProps)=>
                     <TableRow key={item.id}>
                         <TableCell className="bg-fundoSecundairo text-textoPrincipal px-10 py-5 border-y-2 border-l-2 border-borda">
                             <div className="flex items-center space-x-7">
@@ -110,6 +126,21 @@ export default function TableItem(){
                     </TableRow>
                     )}
                 </TableBody>
+                <TableFooter>
+                    <TableCell colSpan={3} className="px-10 py-5 border-2 border-borda bg-fundoSecundairo text-textoPrincipal">
+                        <div className="flex items-center justify-center">
+                            <button onClick={prevPage} className="cursor-pointer disabled:text-textoDesabilitado" disabled={currentPage ===1}>
+                                <ChevronLeft/>
+                            </button>
+                            {Array.from({length:totalPages}, (_,i) => i+1).map((page) => (
+                                <button className={` w-10 h-10 cursor-pointer rounded-full ${currentPage === page ? "text-textoPrincipal border-2 border-primaria text-lg" : "text-textoDesabilitado"} `} key={page} onClick={() => setCurrentPage(page)}>{page}</button>
+                            ))}
+                            <button onClick={nextPage} className="cursor-pointer disabled:text-textoDesabilitado" disabled={currentPage === totalPages || totalPages === 0}>
+                                <ChevronRight/>
+                            </button>
+                        </div>
+                    </TableCell>
+                </TableFooter>
             </Table>
     )
 }
